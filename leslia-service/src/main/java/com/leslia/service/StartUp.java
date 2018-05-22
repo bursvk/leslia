341ea03d6dd15354b4c541abbff4417b955b9985
leslia <959319414@qq.com>
2018-05-22 10:24:03 +0800

    启动类修复

--- leslia-service/src/main/java/com/leslia/service/StartUp.java
+++ leslia-service/src/main/java/com/leslia/service/StartUp.java
@@ -1,13 +1,28 @@
 package com.leslia.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class StartUp {
 
-    public static void main(String args[]) throws  Exception{
-        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext(new String[]{"dubbo-provider.xml"});
-        context.start();
-        System.out.print("服务已启动.....");
-        System.in.read();
+    private static Logger logger= LoggerFactory.getLogger(StartUp.class);
+
+    public static void main(String args[]){
+        try{
+            ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext(new String[]{"dubbo-provider.xml"});
+            context.start();
+        }catch (Exception e){
+            logger.info("== dubbo provider context start error:",e);
+        }
+        synchronized (StartUp.class){
+            while(true){
+                try{
+                    StartUp.class.wait();
+                }catch (InterruptedException e){
+                    logger.error("== synchronized error:",e);
+                }
+            }
+        }
     }
 }
