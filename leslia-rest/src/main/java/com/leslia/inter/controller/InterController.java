3ecfcb9122c8a96fb1ad545b95979df24a2b791c
leslia <959319414@qq.com>
2018-05-24 10:29:05 +0800

    add leslia-rest

--- leslia-rest/src/main/java/com/leslia/inter/controller/InterController.java
+++ leslia-rest/src/main/java/com/leslia/inter/controller/InterController.java
@@ -1,4 +1,42 @@
 package com.leslia.inter.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.leslia.inter.api.InitDemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/inter")
 public class InterController {
+
+    private Logger logger= LoggerFactory.getLogger(InterController.class);
+
+    @Reference
+    private InitDemoService initDemoService;
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(@RequestParam String name){
+        logger.info("name:{}",name);
+        String backName="";
+        try{
+            backName=initDemoService.helloWorld(name);
+            logger.info("backName:{}",backName);
+        }catch (Exception e){
+            logger.error("this witch dubbo service error",e);
+        }
+        return backName;
+    }
+
+
+
 }
